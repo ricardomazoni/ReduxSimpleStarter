c20a9eae6921905a3c59bddc6b81bc4d1b409c47
Ricardo Mazoni <mazoni@engebras.com.br>
2018-05-10 11:59:43 -0300

    inclusao do search Bar

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// import App from './components/app';
+// import reducers from './reducers';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+// ------
+const API_KEY = 'AIzaSyAsLBCuw3JnY7kHyeFjQN7N9Dt9vXEIjUU'
+
+const App= () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>    
+
+  )
+}
+// ---------
+ReactDOM.render(<App />, document.querySelector('.container'))
+// ---------
+
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>
+//   , document.querySelector('.container'));
