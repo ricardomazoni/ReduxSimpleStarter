73c186bb79807780e378496945cddbfee1de3bee
Ricardo Mazoni <mazoni@engebras.com.br>
2018-05-10 17:34:52 -0300

    Configurando a chamada do Video

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { term: ''}
+
+    }
     render() {
-        return <input />
+        // return <input onChange={this.onInputChange} />
+        // return <input onChange={(event) => console.log(event.target.value)} />
+        // return <input onChange={event => console.log(event.target.value)} />
+
+        return (
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} 
+                />
+                {/* Entrada do input: {this.state.term} */}
+            </div>
+        )
     }    
+
+    onInputChange(event) {
+        console.log('Quantidade Digitado: '+  event.target.value.length + ' Conteudo digitado: ' + event.target.value)
+    }
 }
+
 export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+
+import YTSearch from 'youtube-api-search'
+import { runInThisContext } from 'vm';
 
 // import App from './components/app';
 // import reducers from './reducers';
 
 // const createStoreWithMiddleware = applyMiddleware()(createStore);
 // ------
-const API_KEY = 'AIzaSyAsLBCuw3JnY7kHyeFjQN7N9Dt9vXEIjUU'
+const API_KEY = 'AIzaSyDgQ-pndLxBzhFsOrzxLtvLNvK7YMfW8Is'
 
-const App= () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>    
 
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+      // this.setState({ videos: videos })
+    })
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={ this.state.videos} />
+      </div>    
+    )
+  }
 }
 // ---------
 ReactDOM.render(<App />, document.querySelector('.container'))
